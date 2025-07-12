9970e21eeeb4228ad46cd0054296ce9810577d26
Matheus Caetano <matheuseprofissional@gmail.com>
2025-07-12 12:28:01 -0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const (
+	OrderStatusPending   = "pending"
+	OrderStatusShipped   = "shipped"
+	OrderStatusDelivered = "delivered"
+	OrderStatusCanceled  = "canceled"
+)
+
+type Order struct {
+	ID     int
+	Status string
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+	orders := generateOrders(20)
+	fmt.Println("Orders generated:", len(orders))
+
+	go processOrders(orders, &wg)
+	go setOrdersShipped(orders, &wg)
+	go setOrdersDelivered(orders, &wg)
+
+	wg.Wait()
+
+	fmt.Println("Orders processed:", len(orders))
+}
+
+func processOrders(orders []*Order, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		fmt.Println("Order", order.ID, "processed")
+	}
+}
+
+func setOrdersShipped(orders []*Order, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		fmt.Println("Order", order.ID, "shipped")
+		order.changeStatus(OrderStatusShipped)
+	}
+}
+
+func setOrdersDelivered(orders []*Order, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		fmt.Println("Order", order.ID, "delivered")
+		order.changeStatus(OrderStatusDelivered)
+	}
+}
+
+func generateOrders(count int) []*Order {
+	orders := make([]*Order, count)
+	for i := range orders {
+		orders[i] = &Order{
+			ID:     i + 1,
+			Status: OrderStatusPending,
+		}
+	}
+	return orders
+}
+
+func (o *Order) changeStatus(status string) {
+	o.Status = status
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matheuscaet/go-routines
+
+go 1.23.4
